cddebf334f2b0dd4103852103c25bb1efb112439
Turgut Köksal <turgut_koksal_988@hotmail.com>
2026-07-11 15:00:41 +0300

    fix

--- src/data/techIcons.ts
+++ src/data/techIcons.ts
@@ -4,12 +4,6 @@ import tailwindIcon from "../assets/tech/tailwindcss.svg";
 import viteIcon from "../assets/tech/vite.svg";
 import dotnetIcon from "../assets/tech/dotnet.svg";
 import postgresqlIcon from "../assets/tech/postgresql.svg";
-import pythonIcon from "../assets/tech/python.svg";
-import kotlinIcon from "../assets/tech/kotlin.svg";
-import redisIcon from "../assets/tech/redis.svg";
-import bootstrapIcon from "../assets/tech/bootstrap.svg";
-import csharpIcon from "../assets/tech/csharp.svg";
-import windowsIcon from "../assets/tech/windows.svg";
 
 export const homeTechStack = [
   { name: "React", icon: reactIcon },
